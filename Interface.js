75cd1a8549d096020b6b6c1d6de0661c814f6413
Kassemsan01 <kassemsantos29@icloud.com>
2023-09-05 00:19:10 -0300

    mudancas_05

--- Interface.js
+++ Interface.js
@@ -10,3 +10,41 @@ btn.addEventListener('click', ()=>{
         inputSenha.setAttribute('type', 'password')
     }
 })
+
+function entrar(){
+    let usuario = document.querySelector("#usuario")
+    let userLabel = document.querySelector("#userLabel")
+
+    let senha = document.querySelector("#senha")
+    let senhaLabel = document.querySelector("#senhaLabel")
+
+    let msgError = document.querySelector("#msgError")
+    let listaUser = []
+    
+    let userValid = {
+        nome: "",
+        user: "",
+        senha: ""
+    }
+
+    listaUser = JSON.parse(localStorage.getItem("listaUser"))
+
+    listaUser.forEach((item) => {
+        if(usuario.value == item.userCad && senha.value == item.senhaCad){
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+
+        }
+    })
+
+    if(usuario.value == userValid.user && senha.value == userValid.senha){
+
+
+    } else{
+        userLabel.setAt
+
+    }
+}
