30cf3159d86487ccd532cee0cf586cc5f5dacf51
Kassemsantos <kassem.santos@fatec.sp.gov.br>
2023-09-16 20:26:53 -0300

    mudanças

--- Tela_inicio.js
+++ Tela_inicio.js
@@ -5,11 +5,11 @@ logado.innerHTML = "Olá " + userLogado.nome
 
 if(localStorage.getItem("token") == null){
     alert("Você precisa estar logado para acessar essa página")
-    window.location.href = "http://127.0.0.1:5500/Interface.html"
+    window.location.href = "https://kassemsan01.github.io/SafeU/Interface.html"
 }
 
 function sair(){
     localStorage.removeItem("token")
     localStorage.removeItem("userLogado")
-    window.location.href = "Interface.html"
+    window.location.href = "https://kassemsan01.github.io/SafeU/Interface.html"
 }
